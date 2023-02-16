594719f37f41d7bc2a182f846f67431d614096f1
Robin500818167 <robin.van.houten@hva.nl>
2023-02-16 16:07:24 +0100

    week 2 opdracht templating

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 
-const express = require("express");
-const server = express();
-const port = 1337;
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 1337
+const Handlebars = require("handlebars")
 
-server.use('/static', express.static('public'));
+app.use('/static',express.static('static'))
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-server.get("/", (req, res) => {
-  res.end("Hello World");
-});
-server.get("/about", (req, res) => {
+
+app.get('/', (req, res) => { 
+  res.render('main', {
+      profile: 
+      {
+          username: "Nico di Angelo",
+          email: "nicodiangelo@gmail.com",
+          follow: "114 followers   98 following"
+      }
+  })
+  })
+
+app.get("/about", (req, res) => {
   res.end("About me");
 });
-server.get("*", (req, res) => {
-  res.end("404 Error - Page not found");
-});
 
-server.listen(port, () => {
-console.log("Server is running on port 1337");
-});
+app.get('*', (req, res) => {
+    res.send('Not found..')
+})
+
+app.listen(PORT)  
