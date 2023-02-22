078ec97ce8bc7f7e994e7071a20982239dfd8034
Robin500818167 <robin.van.houten@hva.nl>
2023-02-22 11:49:31 +0100

    http

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 1337
-const Handlebars = require("handlebars")
-
-app.use('/static',express.static('static'))
-app.set('view engine', 'hbs')
-app.set('views', 'views')
-
-
-app.get('/', (req, res) => { 
-  res.render('main', {
-      profile: 
-      {
-          username: "Nico di Angelo",
-          email: "nicodiangelo@gmail.com",
-          follow: "114 followers   98 following"
-      }
-  })
-  })
-
-app.get("/about", (req, res) => {
-  res.end("About me");
+const express = require('express')
+const { engine } = require('express-handlebars');
+const app = express();
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 1337
+// create application/json parser
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.engine('.hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'index'
+}));
+app.set('view engine', '.hbs');
+app.set("views", "./views");
+app.get('/', (req, res) => {
+  res.render('main');
 });
-
-app.get('*', (req, res) => {
-    res.send('Not found..')
+app.get('/registreren', (req, res) => {
+  res.render('registreren');
+});
+// bodyparser codes 
+app.post ('/inloggen', urlencodedParser, (req, res) => {
+  res.send('Gebruikersnaam: ' + req.body.username + '<br>Wachtwoord: ' + req.body.password)
 })
 
-app.listen(PORT)  
+app.post ('/registreren', urlencodedParser, (req, res) => {
+  res.send('Gebruikersnaam: ' + req.body.username + '<br>Email: ' + req.body.email + '<br>Wachtwoord: ' + req.body.password)
+})
+
+app.listen(port);
